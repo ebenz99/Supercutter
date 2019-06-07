8dea6ba21a063d50e7256cae31dacc24c93ae8fc
Ethan Bensman <ebenz99@gmail.com>
2019-06-07 19:27:27 -0400

    transcriber remembers words

--- transcribe.py
+++ transcribe.py
@@ -1,11 +1,14 @@
 import os
 from pocketsphinx import AudioFile
+from pprint import pprint
 
+
+stre = ""
 print(os.getcwd()+"/audio")
 for file in os.listdir(os.getcwd()+"/audio"):
 	if file.endswith("wav"):
 		print(file)
 		for phrase in AudioFile(audio_file=(os.getcwd()+"/audio/"+file)):
-			print(phrase) # => "go forward ten meters"
-			print(type(phrase))
+			stre+=str(phrase)
+			print(stre)
 		exit(1)
